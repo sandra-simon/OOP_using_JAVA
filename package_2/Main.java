9a0acb56acf446a4dfdb331c372e08eb1343959c
Sandra Simon <sandrasimon2301@gmail.com>
2025-01-05 12:47:06 +0530

    Update Main.java

--- package_2/Main.java
+++ package_2/Main.java
@@ -1,3 +1,5 @@
+/*Consider a scenario where an interface named ‘Animal’ is in package named ‘animalpackage ’ and it declares two methods ‘get_cries’ and ‘get_eating_type’ that are place holders for printing cry and eating type of animals. The subpackages of ‘animalpackage ’ are ‘herbivorous’and ‘carnivorous’. The ‘herbivorous’ package contain two classes ‘cow’ and ‘elephant’ that implements the ‘Animal’ interface.Similarly the ‘carnivorous’ package contain ‘lion’ and ‘bear’ that too implements the interface. Design a main class that import these classes and invokes the corresponding methods.*/
+
 import animalpackage.herbivorous.Cow;
 import animalpackage.herbivorous.Elephant;
 import animalpackage.carnivorous.Lion;
